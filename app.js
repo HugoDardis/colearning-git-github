8d9d698035fa4a8ffa67409dcc22497d6228f08f
HLPD <hugolpd@gmail.com>
2022-02-18 11:21:22 -0300

    porsição por posição

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 let paises = [];
 
-paises.push("Brasil ", "Argentina ", "Colombia ", "Equador");
+paises.push("Brasil ", "Argentina ", "Colombia ", "Equador ", "Alemanha ", "Espanha ");
 
 
 console.table(paises);
 
+console.log("-")
+
 console.log('imprimindo posição por posição');
+
+//forma tradicional
+for (let i = 0; i < paises.length; i++){
+  console.log(paises[i]);
+}
+
+console.log("-")
+
+//intera
+paises.forEach((pais) => {
+  return console.log(pais)
+})
+
+console.log("-")
+
+//interna sobre array
+for (let pais of paises){
+  console.log(pais)
+}
