17a24dd65e313f0e559ed840257f4f37ed164292
HLPD <hugolpd@gmail.com>
2022-02-18 11:49:29 -0300

    Adicionei linhas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ paises.push("Brasil ", "Argentina ", "Colombia ", "Equador ", "Alemanha ", "Espa
 
 console.table(paises);
 
-console.log("-")
+console.log('-'.repeat(100))
 
 console.log('imprimindo posição por posição');
 
@@ -13,14 +13,14 @@ for (let i = 0; i < paises.length; i++){
   console.log(paises[i]);
 }
 
-console.log("-")
+console.log('-'.repeat(100))
 
 //intera
 paises.forEach((pais) => {
   return console.log(pais)
 })
 
-console.log("-")
+console.log('-'.repeat(100))
 
 //interna sobre array
 for (let pais of paises){
